01812fa477dd6beb56897d3e030a102673d589ba
Martiban <martiban@gmail.com>
2025-10-09 16:52:29 +0200

    Creacion basica de la clase Club

---
+++ Liga/Club.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liga
+{
+    internal class Club
+    {
+        // Atributos
+        string nombre;
+        int añoFundacion;
+
+        // Propiedades
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public int AñoFundacion { get { return añoFundacion; } set { añoFundacion = value; } }
+
+        //Constructores
+        public Club() { }
+
+        // Métodos
+
+    }
+}
